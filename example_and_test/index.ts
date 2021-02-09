d1da46af5c18f49f31490a668d63aa26adf1cbb2
DevilLordAero <redeagledeveloper1@gmail.com>
2021-02-09 20:49:50 +0700

    Disable all other scripts

--- example_and_test/index.ts
+++ example_and_test/index.ts
@@ -2,16 +2,4 @@
 // Console Output
 console.log("From Script> Hello, World!");
 
-import './test';
-import './addonscript';
-import './customcommand';
-import './net-chat';
-import './net-login';
-import './net-printall';
-import './net-rawpacket';
-import './net-disconnect';
-import './net-transferserver';
-import './lowlevel-dll';
-import './lowlevel-apihooking';
-import './lowlevel-nativeclass';
-import './lowlevel-assembler';
+import './CEconomy/EconomyAPI'
